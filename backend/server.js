0a794b676b8b610263aee86057d44fe8d34a1b62
Sahil Sinnarkar <sahilsinnarkar@gmail.com>
2024-12-30 19:29:26 +0530

    1- post routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postsRoutes from "./routes/post.routes.js";
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
